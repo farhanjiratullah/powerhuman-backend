72e29fe7ff3abef36b2625a9430e18176cb6a91d
farhanjiratullah <farhanjiratullah794@gmail.com>
2023-08-21 21:23:16 +0700

    feat: Adjust ResponseFormatter, register, login, fetch user, and logout api from breeze package, and its endpoints as well

--- app/Helpers/ResponseFormatter.php
+++ app/Helpers/ResponseFormatter.php
@@ -12,7 +12,7 @@ class ResponseFormatter
      *
      * @var array
      */
-    protected static $response = [
+    protected static $successResponse = [
         'meta' => [
             'code' => 200,
             'status' => 'success',
@@ -21,28 +21,36 @@ class ResponseFormatter
         'data' => null,
     ];
 
+    protected static $errorResponse = [
+        'meta' => [
+            'code' => 400,
+            'status' => 'error',
+            'message' => null,
+        ],
+        'errors' => null,
+    ];
+
     /**
      * Give success response.
      */
     public static function success($data = null, $message = null, $code = 200)
     {
-        self::$response['meta']['code'] = $code;
-        self::$response['meta']['message'] = $message;
-        self::$response['data'] = $data;
+        self::$successResponse['meta']['code'] = $code;
+        self::$successResponse['meta']['message'] = $message;
+        self::$successResponse['data'] = $data;
 
-        return response()->json(self::$response, self::$response['meta']['code']);
+        return response()->json(self::$successResponse, self::$successResponse['meta']['code']);
     }
 
     /**
      * Give error response.
      */
-    public static function error($data = null, $message = null, $code = 400)
+    public static function error($errors = null, $message = null, $code = 400)
     {
-        self::$response['meta']['status'] = 'error';
-        self::$response['meta']['code'] = $code;
-        self::$response['meta']['message'] = $message;
-        self::$response['data'] = $data;
+        self::$errorResponse['meta']['code'] = $code;
+        self::$errorResponse['meta']['message'] = $message;
+        self::$errorResponse['errors'] = $errors;
 
-        return response()->json(self::$response, self::$response['meta']['code']);
+        return response()->json(self::$errorResponse, self::$errorResponse['meta']['code']);
     }
 }
--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
 
@@ -18,34 +21,44 @@ class RegisteredUserController extends Controller
     /**
      * Display the registration view.
      */
-    public function create(): View
-    {
-        return view('auth.register');
-    }
+    // public function create(): View
+    // {
+    //     return view('auth.register');
+    // }
 
     /**
      * Handle an incoming registration request.
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request): JsonResponse
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        if ($validator->fails()) {
+            return ResponseFormatter::error($validator->errors(), 'Request errors', 422);
+        }
 
-        event(new Registered($user));
+        $validated = $validator->validated();
+        $validated['password'] = Hash::make($validated['password']);
 
-        Auth::login($user);
+        $user = User::create($validated);
 
-        return redirect(RouteServiceProvider::HOME);
+        $token = $user->createToken("token")->plainTextToken;
+
+        // event(new Registered($user));
+
+        return ResponseFormatter::success([
+            'user' => $user,
+            'token' => [
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'expires_in' => config('sanctum.expiration') . " minutes"
+            ]
+        ], 'Successfully registered a new user');
     }
 }
